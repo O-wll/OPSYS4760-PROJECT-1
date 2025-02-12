f1e528ecb508ea395e7c8b71077aaf1a7c8dc9b0
dat nguyen <dat@opsys.cs.umsl.edu>
2025-02-12 15:17:50 -0600

    user.c now prints out user PID and parent PID and what iteration it is before sleeping and afterwards.

--- user.c
+++ user.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 // Author: Dat Nguyen
 // Date: 2/12/2025
 // user.c is a program that is called from oss.c and takes in an integer argument and do iterations based on the input. The iterations display the user PID and the parents PID before sleeping and then after sleeping.
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) { // Main program
+	
 	int calls = 0;
+	int iterations = 0;
 
 	// Input validation
 	if (argc < 2) {
@@ -20,6 +23,14 @@ int main(int argc, char **argv) {
 	if (calls <= 0) {
 		printf("Error: Please input a positive integer. \n");
 		return(EXIT_FAILURE);
+	}
+	
+	// For loop prints the user pid and the parent pid before sleeping, then the same info after sleeping.
+	for (int i = 0; i < calls; i++) {
+		iterations++;
+		printf("USER PID: %d PPID: %d Iteration: %d before sleeping. \n", getpid(), getppid(), iterations);
+		sleep(1);
+		printf("USER PID: %d PPID: %d Iteration: %d after sleeping. \n", getpid(), getppid(), iterations);
 	}
 
 	return 0;
